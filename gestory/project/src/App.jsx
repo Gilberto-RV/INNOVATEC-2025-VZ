5c0230442c41bc6526199086dcd1ed7948b1c188
Gilberto-RV <soul088eater@gmail.com>
2025-11-21 17:07:30 -0600

    Integridad de archivos RSD

--- gestory/project/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import { AppRouter } from './router/AppRouter.jsx';
-import './styles/globals.scss';
-
-function App() {
-  return <AppRouter />;
-}
-
-export default App;
--- gestory/project/src/infrastructure/repositories/CategoryRepository.js
+++
@@ -1,9 +0,0 @@
-// repositories/CategoryRepository.js
-import { httpClient } from '../http/HttpClient.js';
-
-export class CategoryRepository {
-  async getAll() {
-    const response = await httpClient.get('/categories');
-    return response.data;
-  }
-}
